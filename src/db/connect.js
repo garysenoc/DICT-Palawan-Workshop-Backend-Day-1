5225b916af4f25ae60fb3afece2284d4601b808a
Nestot Kenneth Aniar <ken.aniar.dev@gmail.com>
2022-06-21 10:57:15 +0800

    added online db

--- src/db/connect.js
+++ src/db/connect.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 
 const connect = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/my-first-backend");
+    // await mongoose.connect("mongodb://localhost:27017/my-first-backend");
+    await mongoose.connect(
+      "mongodb+srv://ken_user:9rfHqDhmqv134GYC@cluster0.iso4k.mongodb.net/dictworkshop-backend?retryWrites=true&w=majority"
+    );
+    // mongodb compass link
+    // mongodb+srv://ken_user:9rfHqDhmqv134GYC@cluster0.iso4k.mongodb.net/dictworkshop-backend
     console.log("database connected successfully");
   } catch (err) {
     console.error(err);
@@ -11,4 +16,3 @@ const connect = async () => {
 };
 
 module.exports = connect;
-
